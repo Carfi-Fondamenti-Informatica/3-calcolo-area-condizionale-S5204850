86b2e22d51711ffc512409e84bba2096843e8807
S5204850 <100359998+S5204850@users.noreply.github.com>
2022-03-29 15:05:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-   return 0;
+    float a,b; int opzione;
+    std::cin>>a>>b>>opzione;
+    switch (opzione){
+        case 0:
+           std::cout<< (a*b)/2<<std::endl;
+            break;
+        case 1:
+           std::cout<< a*a<<std::endl;
+            break;
+        case 2:
+            std::cout<<a*b<<std::endl;
+            break;
+        default:
+            std::cout<<"opzione non valida"<<std::endl;
+
+
+    }
+
+
+    return 0;
+
 }
+
